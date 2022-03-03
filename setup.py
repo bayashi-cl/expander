b5dd6cc0016f15741906c57c0a92a83d0b9ee4e2
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-03-03 20:34:01 +0900

    update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-import re
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("src/expander/__init__.py", "r") as fd:
-    version = re.search(  # type: ignore
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
-    ).group(1)
-
-setup(
-    name="expander",
-    version=version,
-    author="Masaki Kobayashi",
-    author_email="bayashi.cl@gmail.com",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    license="CC0",
-)
+setup()
